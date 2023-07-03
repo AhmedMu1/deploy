e3ce662334d86466f1d2207be99b2619d493a84b
AhmedMu1 <94929080+AhmedMu1@users.noreply.github.com>
2023-07-03 21:39:59 +0300

    Update app.py

--- deploy/app.py
+++ deploy/app.py
@@ -1,13 +1,7 @@
 from flask import Flask, request, render_template, url_for, jsonify
-from extractPDF import Extract
-from sentenceTransformers import Similarity
 import os
 
-extractPDF = Extract()
-similarity = Similarity()
-
 app = Flask(__name__)
-
 
 @app.route('/listApi', methods=["GET"])
 def list_all_job_descriptions_text():
