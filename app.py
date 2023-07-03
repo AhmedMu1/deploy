324d0a6667a36ba1a0cf079339f46ef4605ab59b
AhmedMu1 <ahmedmagition22@gmail.com>
2023-07-03 21:25:37 +0300

    new deploy
    new

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template, url_for, jsonify
+from extractPDF import Extract
+from sentenceTransformers import Similarity
+import os
+
+extractPDF = Extract()
+similarity = Similarity()
+
+app = Flask(__name__)
+
+
+@app.route('/listApi', methods=["GET"])
+def list_all_job_descriptions_text():
+    # Set the path to the folder containing the TXT files
+    folder_path = 'Job_description_TXT'
+
+    # Get a list of all the files in the folder
+    files = os.listdir(folder_path)
+
+    # Filter the list to include only TXT files
+    txt_files = [file for file in files if file.endswith('.txt')]
+
+    # Join the names of the TXT files using a delimiter
+    job_description_names = "\n".join(txt_files)
+
+    return job_description_names
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
